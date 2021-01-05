5c3ecc90d1ec0a571db578d62d3675cdb88be734
Jonathan <>
2021-01-05 17:04:59 -0500

    cleaned up pigLatinBest

--- PigLatin.java
+++ PigLatin.java
@@ -1,11 +1,23 @@
+import java.util.*;
 public class PigLatin{
   public static void main(String[] args) {
-    System.out.println(PigLatin.pigLatinSimple("oink"));
+    /*System.out.println(PigLatin.pigLatinSimple("oink"));
     System.out.println(PigLatin.pigLatinSimple("mock"));
     System.out.println(PigLatin.pigLatin("the"));
     System.out.println(PigLatin.pigLatin("oink"));
     System.out.println(PigLatin.pigLatinBest("*emu"));
-    System.out.println(PigLatin.pigLatinBest("fish!"));
+    System.out.println(PigLatin.pigLatinBest("fish!")); */
+    Scanner text = new Scanner(System.in);
+    while(text.hasNextLine()){
+      Scanner line = new Scanner(text.nextLine());
+      while(line.hasNext()){
+        System.out.print(pigLatinBest(line.next()));
+        if(line.hasNext()){
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
   }
   public static String pigLatinSimple(String s){
     int length = s.length();
@@ -25,6 +37,9 @@ public class PigLatin{
                        "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk",
                        "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
     int length = digraphs.length;
+    if(s.length() < 3){
+      return pigLatinSimple(s);
+    }
     for(int i = 0;i < length;i++){
       if(s.substring(0, 2).equals(digraphs[i])){
         return s.substring(2, s.length()) + digraphs[i] + "ay";
@@ -34,18 +49,16 @@ public class PigLatin{
     }
 
   public static String pigLatinBest(String s){
-    String special = "";
+    s = s.toLowerCase();
     int length = s.length();
-    for(int i = 0;i < s.length();i++){
-      if(!Character.isDigit(s.charAt(i)) && !Character.isLetter(s.charAt(i))){
-        special += s.charAt(i);
-      }
+    if(length == 1){
+      return s;
     }
     if(!Character.isDigit(s.charAt(0)) && !Character.isLetter(s.charAt(0))){
       return s;
     }
-    else if(Character.isLetter(s.charAt(0))){
-      return pigLatin(s.substring(0, length - 1)) + special;
+    else if(!Character.isLetter(s.charAt(length - 1)) && !Character.isDigit(s.charAt(length - 1))) {
+      return pigLatin(s.substring(0, length - 1)) + s.substring(length - 1, length);
     }
     return pigLatin(s);
     }
